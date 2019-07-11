8ea163c9d519704711d1cbb47cc2a8d198db99a3
Fnaz40 <48803891+Fnaz40@users.noreply.github.com>
2019-07-11 18:00:19 +0500

    WIP

---
+++ script.js
@@ -0,0 +1,37 @@
+// TECHKARO data base.
+let techkaro = {
+    courseName = "Web-development",
+    students = [
+        {
+            name: "sofia",
+            studentCNic: 42401-565565-5,
+            sstudentContact: 03426537,
+            fatherName: "Hafeez",
+            fatheCNic: 42401-5897965-4,
+            fatherContact: 03524624545,
+            qualification: "metric",
+            dob: 14-05-1999,
+            gander: "female",
+            noOfSibllings: 5,
+            hobbies: ["football player", "hockey"],
+            isemployes: false,
+            fatheroccuay: "maneger",
+            fatherInCome: 30000,
+            persnolInCome: 10000,
+            email: "sofia444@gmail.com",
+            rollOn: 10,
+            group: "A",
+            address: "abc road site karachi",
+            distance: "10km",
+            extraSkills: ["Art", "singing"],
+            profession: "nothing",
+            otherCourse: {
+                courseName = "Beautision",
+                institute = "memonfoundation",
+                timing = "4PM-6PM",
+                distance = "15km",
+            },
+            traveling: "bus"
+        }
+    ]
+}
